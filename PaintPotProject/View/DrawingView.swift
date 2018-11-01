3aed9f395a5e4be15605be61dba80842d16cb1fe
Jenna Mills <superangelfish555@gmail.com>
2018-11-01 09:43:41 -0600

    Created drawTurtle(), created myDrawing()

--- PaintPotProject/View/DrawingView.swift
+++ PaintPotProject/View/DrawingView.swift
@@ -15,12 +15,78 @@ public class DrawingView: UIView
     public override func draw(_ rect: CGRect) -> Void
     {
         // Drawing code
+        createStickFigure().stroke()
+        drawTurtle()
+        myDrawing()
     }
     
     private func createStickFigure() -> UIBezierPath
     {
         let figure : UIBezierPath = UIBezierPath()
+        
+        UIColor.magenta.setStroke()
+        figure.lineWidth = 3.0
+        
+        figure.addArc(withCenter: CGPoint(x: 200, y:200),
+                                    radius: CGFloat(20),
+                                      startAngle: CGFloat(0),
+                                      endAngle: CGFloat(2) * CGFloat.pi,
+                                      clockwise: true)
+        
+            figure.move(to: CGPoint(x: 200, y: 220))
+            figure.addLine(to: CGPoint(x:200, y: 270))
+            figure.move(to: CGPoint(x: 180, y: 240))
+            figure.addLine(to: CGPoint(x: 220, y: 240))
+            figure.move(to: CGPoint(x:200, y: 270))
+            figure.addLine(to: CGPoint(x: 180, y:300))
+            figure.move(to: CGPoint(x: 200,  y: 270))
+            figure.addLine(to: CGPoint(x: 220, y: 300))
+            
         return figure
     }
+    
+    public func drawTurtle() -> Void
+    {
+        let logo = UIBezierPath()
+        UIColor.white.setFill()
+        logo.move(to: CGPoint(x: 50, y: 250))
+        logo.addLine(to: CGPoint(x: 100, y: 300))
+        logo.addLine(to: CGPoint(x: 50, y: 350))
+        logo.close()
+        logo.fill()
+    }
+    
+    public func myDrawing() -> Void
+    {
+        let coolio = UIBezierPath()
+        UIColor.magenta.setStroke()
+        coolio.move(to: CGPoint(x: 50, y: 400))
+        coolio.addLine(to: CGPoint(x: 30, y: 390))
+        coolio.addLine(to: CGPoint(x: 10, y: 390))
+        coolio.addLine(to: CGPoint(x: -10, y: 400))
+        coolio.move(to: CGPoint(x: 30, y: 420))
+        coolio.addLine(to: CGPoint(x: 30, y: 439))
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
+
